ac2783fe5c8b96b6e9ad29dc1a6a2de12acbe6c0
Dominique K Kitchenakow <domikitchen@users.noreply.github.com>
2020-09-14 15:49:18 -0500

    completed mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,28 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    count = 0
+    for i in arr:
+        if arr[count] == target:
+            return count
+        count += 1
     return -1   # not found
-
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        midpoint = (right + left) // 2
 
-    # Your code here
+        if arr[midpoint] == target:
+            return midpoint
 
+        elif arr[midpoint] < target:
+            left = midpoint + 1
 
+        else:
+            right = midpoint - 1
     return -1  # not found
+
+arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+
+print(binary_search(arr1, 98))
